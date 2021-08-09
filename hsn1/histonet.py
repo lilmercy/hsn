5df0310008bf5d654d102b3346d2e666d3de3e7c
lilmercy <80046838+lilmercy@users.noreply.github.com>
2021-08-09 20:59:15 +0800

    Update histonet.py
    add annotations

--- hsn1/histonet.py
+++ hsn1/histonet.py
@@ -6,40 +6,52 @@ from keras import optimizers
 import scipy
 from scipy import io
 
+# 定义一个实现分类CNN阶段的类（HistoNet）
 class HistoNet:
     """Class for implementing the classification CNN stage (HistoNet)"""
-
+    # 初始函数
     def __init__(self, params):
-        # Set constant parameters
-        self.train_mean = 193.09203
-        self.train_std = 56.450138
+        # 设置常量参数
+        self.train_mean = 193.09203 # 训练集均值
+        self.train_std = 56.450138 # 训练集方差
 
-        # Set user-defined parameters
+        # S设置用户自定义参数
         self.model_dir = params['model_dir']
         self.model_name = params['model_name']
         self.batch_size = params['batch_size']
         self.relevant_inds = params['relevant_inds']
         self.input_name = params['input_name']
         self.class_names = params['class_names']
-
+    
+    # 从文件加载模型架构、权重并编译模型
     def build_model(self):
         """Load model architecture, weights from file and compile the model"""
-
-        # Load architecture from json
+       
+        # 从 json 文件加载模型
         model_json_path = os.path.join(self.model_dir, self.model_name + '.json')
         json_file = open(model_json_path, 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         self.model = model_from_json(loaded_model_json)
 
-        # Load weights from h5
+        # 从 h5 文件加载权重
         model_h5_path = os.path.join(self.model_dir, self.model_name + '.h5')
         self.model.load_weights(model_h5_path)
 
-        # Evaluate model
-        opt = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+        # 评估模型
+        # SGD -- 随机梯度下降优化器
+        # optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
+        # lr -- 学习率（大于或等于零的浮点数）, momentum -- 动量参数（大于或等于零的浮点数）
+        # decay -- 学习率衰减值（大于或等于零的浮点数）, nesterov -- 确定是否使用Nesterov动量（布尔值）
+        opt = optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True) 
+        # loss -- 损失函数 -- 二元交叉熵, optimizer -- 优化器 -- opt(SGD), metrics -- 准确率 -- 二元准确率
+        # keras.metrics.binary-aacuracy(y_true, y_pred, threshoud=0.5)
+        # 计算方法: 比如有6个样本，其y_true为[0, 0, 0, 1, 1, 0]，y_pred为[0.2, 0.3, 0.6, 0.7, 0.8, 0.1]，那么其binary_accuracy=5/6=87.5%。
+        # 1）将y_pred中的每个预测值和threshold对比，大于threshold的设为1，小于等于threshold的设为0，得到y_pred_new=[0, 0, 1, 1, 1, 0]；
+        # 2）将y_true和y_pred_new代入到计算中得到最终的binary_accuracy=87.5%。
         self.model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['binary_accuracy'])
 
+    # 讲输入图像标准化（归一化）
     def normalize_image(self, X, is_glas=False):
         """Normalize the input images
 
